6540c234a9aae81fbaa509558a90e1260ee5f65d
Maxime Leblanc-Gervais <47794795+cptnFlamingo@users.noreply.github.com>
2024-10-05 13:07:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Ceci est un commentaire en javascript
+
+/*
+    Ceci est un commentaire 
+    multi-lignes
+*/
+
+console.log('hello world')
+
+const div = document.getElementById("exemple-id");
+div.innerText = ":)"
